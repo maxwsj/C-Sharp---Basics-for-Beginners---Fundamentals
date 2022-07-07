3cb7f8488c2acb7bdbf32a7cc9e41756cae75c18
maxwsj <mw2study@gmail.com>
2022-07-07 15:15:12 -0300

    Working with dates

---
+++ Working with Dates/DateTimeLesson/DateTimeLesson/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DateTimeLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var dateTime = new DateTime(2015, 1, 1);
+
+            // Getting the current datetime
+            var now = DateTime.Now;
+
+            /*// Getting today's date irrespective of the time
+            var today = DateTime.Today;
+
+            Console.WriteLine($"Today: {today}");
+            Console.WriteLine($"Hour: {now.Hour}");
+            Console.WriteLine($"Minute: {now.Minute}");
+
+            // Dates and Hours in C# are immutable
+
+            // So, how to modify them  ?
+            var tomorrow =  now.AddDays(1); // Returns tomorrow current time
+
+            // Going back in the past dd/mm/yy ?
+            var yesterday = now.AddDays(-1); // Returns yesterday current time*/
+
+            Console.WriteLine(now.ToLongDateString()); // quarta-feira, 6 de julho de 2022
+            Console.WriteLine(now.ToShortDateString()); // 06/07/2022
+            Console.WriteLine(now.ToLongTimeString()); // 23:58:37
+            Console.WriteLine(now.ToShortDateString()); // 06/07/2022
+
+            // What if we wanted to show buff date and time ?
+            Console.WriteLine(now.ToString()); // 07/07/2022 00:01:53
+            Console.WriteLine(now.ToString("yyy-MM-dd HH:mm")); // we can pass the date format that we want inside the parentesis
+        }
+    }
+}
---
+++ Working with Dates/TimeSpanLesson/TimeSpanLesson/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TimeSpanLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /***************** CREATING *****************/
+            ///////////// First way to create a time span ///////////
+            var timeSpan = new TimeSpan(1, 2, 3); // The arguments that we are passing here are: hours, minutes and seconds
+
+            ////////// Second way to create a time span ///////////
+            // What if we didn't have aa value for minutes and seconds ?
+            var timeSpan1 = new TimeSpan(1, 0, 0);
+
+            // A more readble way to create the same time span object, is to use static methods on the time span structure
+            var timeSpanStatic = TimeSpan.FromHours(1);
+
+            ///////////// Third way to create a time span ///////////
+            ///// If we have two Daytime objects and we subtract them, the result is a timespan
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine(duration);
+
+            /***************** PROPERTIES *****************/
+            Console.WriteLine($"Minutes: {timeSpan.Minutes}"); // Returns the minutes component of your time span object
+            Console.WriteLine($"Total Minutes: {timeSpan.TotalMinutes}"); // Converts that timespan object into minutes
+
+            /***************** ADD *****************/
+            Console.WriteLine($"Add Example: {timeSpan.Add(TimeSpan.FromMinutes(8))}"); // We added 8 minutes
+            Console.WriteLine($"Subtract Example: {timeSpan.Subtract(TimeSpan.FromMinutes(8))}"); // We subtracted 8 minutes
+
+            /***************** CONVERTING TO STRING *****************/
+            // ToString
+            Console.WriteLine($"ToString: {timeSpan.ToString()}")
+
+            // Parse
+            Console.WriteLine($"ToString: {TimeSpan.Parse("01:02:03")}");
+
+        }
+    }
+}
